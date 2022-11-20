3cbe7a41e6b23884d5ff78bb98add3afdb3db9ec
aguiardafa <diego.fernandes.aguiar@gmail.com>
2022-11-19 23:56:11 -0300

    More components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Header() {
+    return (
+        <>
+            <header>
+                <div className="dsmeta-logo-container">
+                    <img src={logo} alt="DSMeta" />
+                    <h1>Gerenciador de Vendas</h1>
+                    <p>
+                        Desenvolvido por <b><a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a></b> e customizado por <b><a href="https://github.com/aguiardafa">@aguiardafa</a></b>
+                    </p>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
