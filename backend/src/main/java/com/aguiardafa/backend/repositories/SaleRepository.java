273cd23289bddf62ca9fd5eadba7144df3662cd2
aguiardafa <diego.fernandes.aguiar@gmail.com>
2022-11-20 15:21:37 -0300

    Personalized Query Date Select

--- backend/src/main/java/com/aguiardafa/backend/repositories/SaleRepository.java
+++ backend/src/main/java/com/aguiardafa/backend/repositories/SaleRepository.java
@@ -1,8 +1,17 @@
 package com.aguiardafa.backend.repositories;
 
 import com.aguiardafa.backend.entities.Sale;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    @Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+    Page<Sale> findSales(@Param("min") LocalDate min, @Param("max") LocalDate max, Pageable pageable);
 
 }
--- backend/src/main/java/com/aguiardafa/backend/services/SaleService.java
+++ backend/src/main/java/com/aguiardafa/backend/services/SaleService.java
@@ -7,7 +7,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 @Service
 public class SaleService {
@@ -15,7 +17,12 @@ public class SaleService {
     @Autowired
     private SaleRepository repository;
 
-    public Page<Sale> findSales(Pageable pageable){
-        return repository.findAll(pageable);
+    public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable){
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+        LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
+        return repository.findSales(min, max, pageable);
     }
 }
