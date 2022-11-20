6ec339dd029af647caf752f0a2478125d341b6f7
aguiardafa <diego.fernandes.aguiar@gmail.com>
2022-11-20 01:51:44 -0300

    Pageable List

--- backend/src/main/java/com/aguiardafa/backend/controllers/SaleController.java
+++ backend/src/main/java/com/aguiardafa/backend/controllers/SaleController.java
@@ -3,11 +3,11 @@ package com.aguiardafa.backend.controllers;
 import com.aguiardafa.backend.entities.Sale;
 import com.aguiardafa.backend.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -17,7 +17,7 @@ public class SaleController {
     private SaleService service;
 
     @GetMapping
-    public List<Sale> findSales(){
-        return service.findSales();
+    public Page<Sale> findSales(Pageable pageable){
+        return service.findSales(pageable);
     }
 }
--- backend/src/main/java/com/aguiardafa/backend/services/SaleService.java
+++ backend/src/main/java/com/aguiardafa/backend/services/SaleService.java
@@ -3,6 +3,8 @@ package com.aguiardafa.backend.services;
 import com.aguiardafa.backend.entities.Sale;
 import com.aguiardafa.backend.repositories.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,7 +15,7 @@ public class SaleService {
     @Autowired
     private SaleRepository repository;
 
-    public List<Sale> findSales(){
-        return repository.findAll();
+    public Page<Sale> findSales(Pageable pageable){
+        return repository.findAll(pageable);
     }
 }
